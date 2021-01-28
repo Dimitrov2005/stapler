327b95716f8dfbc99274fd880dfd314348f56840
Dimitrov2005 <s.dimitrov2005@gmail.com>
2021-01-28 19:30:26 +0200

    Initial commit

---
+++ Stapler.java
@@ -0,0 +1,17 @@
+ublic class Stapler {
+    Staple staple;
+    PlasticCase plasticCase;
+    metalParts metalParts;
+
+    public void openUp(void){
+        //Open up the stapler
+    }
+    public Staple pushStaple(staple){
+        //Push staple into the paper
+        return staple.deformed();
+    }
+    public void turnBase(){
+        //From metal parts use turn base
+    }
+}
+p
